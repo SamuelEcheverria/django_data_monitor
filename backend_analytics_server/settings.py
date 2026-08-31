46bd0ee28324740653c5710edd47fefa27cd2af0
Samuel <sechever@espol.edu.ec>
2026-08-31 16:57:07 -0500

    Evitar NoneType en variables de base de datos

--- backend_analytics_server/settings.py
+++ backend_analytics_server/settings.py
@@ -85,11 +85,11 @@ WSGI_APPLICATION = "backend_analytics_server.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQLDATABASE'),
-        'USER': os.environ.get('MYSQLUSER'),
-        'PASSWORD': os.environ.get('MYSQLPASSWORD'),
-        'HOST': os.environ.get('MYSQLHOST'),
-        'PORT': os.environ.get('MYSQLPORT'),
+        'NAME': os.environ.get('MYSQLDATABASE', ''),
+        'USER': os.environ.get('MYSQLUSER', ''),
+        'PASSWORD': os.environ.get('MYSQLPASSWORD', ''),
+        'HOST': os.environ.get('MYSQLHOST', ''),
+        'PORT': os.environ.get('MYSQLPORT', '3306'),
     }
 }
 
